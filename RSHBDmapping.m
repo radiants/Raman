86dacafbd3bf6827d667882ee19ee4221fce6360
Xianze Meng <mengxianze@outlook.com>
2025-01-06 11:03:40 +0800

    Add files via upload

--- RSHBDmapping.m
+++ RSHBDmapping.m
@@ -1,3 +1,7 @@
+% Hydrogen Bond Decoupling for Raman Spectroscopy (HBDRS)
+% Smothing with Wavelet method and Baseline Correction with Quadratic Polynomial Fitting
+% Promoted by Xian-ze MENG, Fa-He Cao*, 28 Dec 2024, Sun Yat-sen University
+
 clear
 close all
 global SpectraType
